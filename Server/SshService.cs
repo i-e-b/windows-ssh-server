cd9ec4301c19819c53933302ddb9dc8af06400e1
Alex Regueiro <alexreg@gmail.com>
2008-02-18 21:50:12 +0000

    Set up framework in code for Windows Service (and service installer) for SSH server.

---
+++ Server/SshService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WindowsSshServer
+{
+    public partial class SshService : ServiceBase
+    {
+        public static new string ServiceName
+        {
+            get { return "WindowsSshServer"; }
+        }
+
+        protected SshTcpServer _tcpServer; // TCP server for SSH connections.
+
+        public SshService()
+            : base()
+        {
+            // Create TCP server.
+            _tcpServer = new SshTcpServer();
+
+            this.Disposed += new EventHandler(SshServerService_Disposed);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Start TCP server.
+            _tcpServer.Start();
+
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            // Stop TCP server.
+            _tcpServer.Stop();
+
+            base.OnStop();
+        }
+
+        protected override void OnPause()
+        {
+            //
+
+            base.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            //
+
+            base.OnContinue();
+        }
+
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            //
+
+            return base.OnPowerEvent(powerStatus);
+        }
+
+        protected override void OnShutdown()
+        {
+            //
+
+            base.OnShutdown();
+        }
+
+        protected override void OnSessionChange(SessionChangeDescription changeDescription)
+        {
+            //
+
+            base.OnSessionChange(changeDescription);
+        }
+
+        protected override void OnCustomCommand(int command)
+        {
+            //
+
+            base.OnCustomCommand(command);
+        }
+
+        private void SshServerService_Disposed(object sender, EventArgs e)
+        {
+            // Dispose TCP server.
+            _tcpServer.Dispose();
+        }
+    }
+}
---
+++ Server/SshServiceInstaller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WindowsSshServer
+{
+    [RunInstaller(true)]
+    public class SshServiceInstaller : Installer
+    {
+        // To Do: make installer specify dependencies in registry.
+        // See bottom of http://www.montgomerysoftware.com/CreatingWindowsServiceInCSharp.aspx.
+        public SshServiceInstaller()
+            : base()
+        {
+            // Add service installer.
+            var serviceInstaller = new ServiceInstaller();
+
+            serviceInstaller.ServiceName = SshService.ServiceName;
+            serviceInstaller.DisplayName = "Windows SSH Server";
+
+            this.Installers.Add(serviceInstaller);
+
+            // Add service process installer.
+            var serviceProcInstaller = new ServiceProcessInstaller();
+
+            serviceProcInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcInstaller.Username = null;
+            serviceProcInstaller.Password = null;
+
+            this.Installers.Add(serviceProcInstaller);
+        }
+
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+    }
+}
